265e5efbd74b2ddcb2d578db8ee9cd52a536c49d
Andrej <andrejtodoroski@yahoo.com>
2024-05-08 11:50:33 -0400

    added more anime and ascii art

--- lib/bot.py
+++ lib/bot.py
@@ -11,7 +11,7 @@ from lib.models import Anime
 from lib.models import Commands
 from lib.models import About_keywords
 
-class AnmieAI:
+class AnmieAI():
     
     asked_questions = []
 
@@ -109,7 +109,7 @@ class AnmieAI:
             else:
                 # /home/jj/Development/code/phase-3/senstive-files/rich-world-329001-e02b32691e8a.json
                 # ''/Users/andrejtodoroski/Development/code/phase-3/sensitive-files/rich-world-329001-e02b32691e8a.json
-                credentials = service_account.Credentials.from_service_account_file('/home/jj/Development/code/phase-3/senstive-files/rich-world-329001-e02b32691e8a.json') 
+                credentials = service_account.Credentials.from_service_account_file('/Users/andrejtodoroski/Development/code/phase-3/sensitive-files/rich-world-329001-e02b32691e8a.json') 
 
                 # Create a Dialogflow client
                 client = dialogflow.SessionsClient(credentials=credentials)
